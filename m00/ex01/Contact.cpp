9fc9a5e5b58baa595504630ee6b61fba62ec4b75
Mac <mac@Macs-MBP.mshome.net>
2021-07-08 23:45:47 +0100

    setsgetsand learning stuff

--- m00/ex01/Contact.cpp
+++ m00/ex01/Contact.cpp
@@ -6,20 +6,40 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 16:36:36 by anassif           #+#    #+#             */
-/*   Updated: 2021/07/08 17:06:46 by anassif          ###   ########.fr       */
+/*   Updated: 2021/07/08 23:45:06 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
+Contact::Contact()
+{
+}
+
+void    Contact::set_first_name(const std::string first_name)
+{
+	this->first_name = first_name;
+}
+
+Contact::~Contact()
+{
+	// std::cout << "i m done" << std::endl;
+}
 int main(void)
 {
 	std::string s;
+	Contact c[8];
+	
+	Contact *con = new Contact();
+	delete con;
+	c[0].set_index(4);
+	c[0].set_first_name("hakim");
 
+	std::cout << c[0].get_first_name() << std::endl;
+	std::cout << c[1].get_index() << std::endl;
 	std::cout << "enter command\n";
 	std::cin >> s;
 	if (s == "EXIT")
 		std::cout << "program exited properly\n";return (0);
-	
 	return (0);
 }
--- m00/ex01/Contact.hpp
+++ m00/ex01/Contact.hpp
@@ -6,14 +6,15 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 16:37:58 by anassif           #+#    #+#             */
-/*   Updated: 2021/07/08 17:35:33 by anassif          ###   ########.fr       */
+/*   Updated: 2021/07/08 22:44:09 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef NOKIAPILL_H
-#define NOKIAPILL_H
+#ifndef Contact_H
+#define Contact_H
 
 #include <iostream>
+#include <string>
 
 class Contact{
     private:
@@ -22,14 +23,27 @@ class Contact{
     std::string last_name;
     std::string nickname;
     public:
-    Contact(){};
+    Contact();
     Contact(int i, std::string first, std::string last, std::string nick){
         index = i;
         first_name = first;
         last_name = last;
         nickname = nick;
     };
-    ~Contact(){};
+    ~Contact();
+    const std::string &get_first_name(void)
+    {
+        return first_name;
+    }
+    void    set_first_name(const std::string first_name);
+    int get_index(void)
+    {
+        return (index);
+    }
+    void set_index(int index)
+    {
+        this->index = index;
+    }
 };
 
 #endif

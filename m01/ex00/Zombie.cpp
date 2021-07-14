f8cb115b78b605baa5bcac5b41667fa9915eb970
Mac <mac@Macs-MBP.mshome.net>
2021-07-14 02:41:07 +0100

    zombie is on

--- m01/ex00/Zombie.cpp
+++ m01/ex00/Zombie.cpp
@@ -1,8 +1,17 @@
 #include "Zombie.hpp"
 
+Zombie::Zombie(std::string name)
+{
+    this->_name = name;
+    this->announce();
+}
 
+Zombie::~Zombie()
+{
+    std::cout << "zombie :" << this->_name << " boom" << std::endl;
+}
 
 void Zombie::announce(void)
 {
-    
+    std::cout << "<" << this->_name << ">" << "BraiiiiiiinnnzzzZ..." << std::endl;
 }

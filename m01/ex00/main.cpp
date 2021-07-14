8f26c472d54b6028157b4d68a17f21f5bdcb1bb3
Abdelhakim Nassif <anassif@e0r2p2.1337.ma>
2021-07-14 20:08:27 +0100

    no need for const test in main

--- m01/ex00/main.cpp
+++ m01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 17:21:48 by anassif           #+#    #+#             */
-/*   Updated: 2021/07/14 17:42:39 by anassif          ###   ########.fr       */
+/*   Updated: 2021/07/14 20:06:58 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 int main (void)
 {
     Zombie *z;
-    Zombie c("yaaaaaa3");
-    
-    c.announce();
     
     z = newZombie("baaaaaa3");
     z->announce();
-    delete(z);
+    
 
     randomChump("euuuuuuuuuu3");
     
-    
-    
+    delete(z);
     return 0;
 }

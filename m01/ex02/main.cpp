4e2496c7d35de0b045de0f3367ac7601fbaa0e1f
Abdelhakim Nassif <anassif@e0r3p3.1337.ma>
2021-07-16 16:43:53 +0100

    cleaning ex02

--- m01/ex02/main.cpp
+++ m01/ex02/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/16 14:40:56 by anassif           #+#    #+#             */
+/*   Updated: 2021/07/16 16:43:22 by anassif          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int main(void)
@@ -6,11 +18,11 @@ int main(void)
     std::string *stringPTR = &s;
     std::string &stringREF = s;
 
-    std::cout << &s << std::endl;
-    std::cout << stringPTR << std::endl;
-    std::cout << &stringREF << std::endl << std::endl;
+    std::cout << "the adress of the string :                "<< &s << std::endl;
+    std::cout << "the adress of the string using stringPTR :"<< stringPTR << std::endl;
+    std::cout << "the adress of the string using stringREF :"<< &stringREF << std::endl << std::endl;
 
-    std::cout << *stringPTR << std::endl;
-    std::cout << stringREF << std::endl;
+    std::cout << "string using pointer   : "<< *stringPTR << std::endl;
+    std::cout << "string using reference : "<< stringREF << std::endl;
     return 0;
 }

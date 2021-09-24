f685b01f4dcecf35762aff0e3115c7ddc727558d
Abdelhakim Nassif <anassif@e0r3p2.1337.ma>
2021-09-24 17:16:47 +0100

    working on ex03

--- m03/ex03/DiamondTrap.cpp
+++ m03/ex03/DiamondTrap.cpp
@@ -6,7 +6,45 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 13:11:33 by anassif           #+#    #+#             */
-/*   Updated: 2021/09/24 13:11:33 by anassif          ###   ########.fr       */
+/*   Updated: 2021/09/24 17:13:31 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap()
+{
+    std::cout << "Default DiamondTrap constractor called\n";
+    this->_Hitpoints = FragTrap::_Hitpoints;
+    this->_Energy_points = ScavTrap::_Energy_points;
+    this->_Attack_damage = FragTrap::_Attack_damage;
+}
+
+DiamondTrap::DiamondTrap(std::string name): _Name(name)
+{
+    std::cout << "DiamondTrap name constructor called\n";
+     this->_Hitpoints = FragTrap::_Hitpoints;
+    this->_Energy_points = ScavTrap::_Energy_points;
+    this->_Attack_damage = FragTrap::_Attack_damage;
+}
+
+DiamondTrap::DiamondTrap(DiamondTrap const &obj)
+{
+    *this = obj;
+    std::cout << "DiamondTrap Copy constructor called\n";
+}
+
+DiamondTrap &DiamondTrap::operator=(DiamondTrap const &obj)
+{
+    std::cout << "DiamondTrap Assignation operator called\n";
+    this->_Name = obj._Name;
+    return *this;
+}
+
+DiamondTrap::~DiamondTrap()
+{
+    std::cout << "DiamondTrap " << this->_Name << " BOOOM\n";   
+}
+
+
 
--- m03/ex03/DiamondTrap.hpp
+++ m03/ex03/DiamondTrap.hpp
@@ -6,7 +6,27 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 13:11:46 by anassif           #+#    #+#             */
-/*   Updated: 2021/09/24 13:11:48 by anassif          ###   ########.fr       */
+/*   Updated: 2021/09/24 17:14:06 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+class DiamondTrap : public FragTrap, public ScavTrap
+{
+    private:
+        std::string _Name;
+        int _Hitpoints;
+        int _Energy_points;
+        int _Attack_damage;
+    public:
+        DiamondTrap();
+        DiamondTrap(std::string name);
+        DiamondTrap(DiamondTrap const &obj);
+        DiamondTrap &operator = (DiamondTrap const &test);
+        ~DiamondTrap();
+        void attack(std::string const & target);
+};

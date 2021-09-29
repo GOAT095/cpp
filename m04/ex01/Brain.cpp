44feb4226d9444a21720bc84ecefd4aa366f6d15
Abdelhakim Nassif <anassif@e0r1p2.1337.ma>
2021-09-29 18:13:12 +0100

    ex01 soon

--- m04/ex01/Brain.cpp
+++ m04/ex01/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 17:26:56 by anassif           #+#    #+#             */
-/*   Updated: 2021/09/29 18:07:43 by anassif          ###   ########.fr       */
+/*   Updated: 2021/09/29 18:12:33 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,18 @@ Brain::Brain()
     std::cout << "Brain Default constructor called\n";
 }
 
-Brain::Brain(const Brain& copy)
+Brain::Brain(const Brain& obj)
 {
     std::cout << "Brain Copy constructor called\n";
-    *this = copy;
+    *this = obj;
 }
 
-Brain& Brain::operator = (const Brain& brain)
+Brain& Brain::operator = (const Brain& obj)
 {
     std::cout << "Brain Assignement constructor called\n";
-    if (this != &brain)
+    for(int i = 0 ; i < 100 ; i++)
     {
-        for(int i = 0 ; i < 100 ; i++)
-        {
-            this->ideas[i] = brain.ideas[i];
-        }
+        this->ideas[i] = brain.ideas[i];
     }
     return *this;
 }
--- m04/ex01/Brain.hpp
+++ m04/ex01/Brain.hpp
@@ -6,26 +6,27 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 17:27:03 by anassif           #+#    #+#             */
-/*   Updated: 2021/09/29 17:27:59 by anassif          ###   ########.fr       */
+/*   Updated: 2021/09/29 18:13:03 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #pragma once
 
-#include "iomanip"
 #include "iostream"
 
 class   Brain
 {
+    protected:
+        std::string ideas[100];
+    
     public:
         Brain();
-        Brain(const Brain& copy);
-        Brain& operator = (const Brain& animal);
+        Brain(const Brain& obj);
+        Brain& operator = (const Brain& obj);
         ~Brain();
         void setIdea(int i, std::string idea);
         std::string getIdea(int i);
 
-    protected:
-        std::string ideas[100];
+    
 };

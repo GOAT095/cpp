ebe4cd07572437a878f54ebe904fdc1fb5969089
Abdelhakim Nassif <anassif@e0r1p2.1337.ma>
2021-10-03 17:08:51 +0100

    eee

---
+++ m05/ex00/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+
+class Bureaucrat{
+    protected:
+        std::string name;
+        int   grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string newname, int newdgrade);
+        Bureaucrat(const Bureaucrat& obj);
+        Bureaucrat& operator = (const Bureaucrat& obj);
+        ~Bureaucrat();
+
+        std::string getName(void) const;
+        int getGrade(void) const;
+
+        void setGrade(int newgrade);
+        void setName(std::string newname);
+
+        class GradeTooHighException :public std::exception
+        {
+            const char * what () const throw () //so it doesnt throw
+            {
+                return "grade is too hight !\n";
+            }
+        };
+        class GradeTooLowException :public std::exception
+        {
+            const char * what () const throw ()
+            {
+                return "grade is too low !\n";
+            }
+        };
+        void gradeInc();
+        void gradeDec();
+        
+};
+
+std::ostream &operator<<(std::ostream & o, Bureaucrat const &rhs);

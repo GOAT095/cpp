8d1420572eccb9412656edd0c67959dea81420e3
Abdelhakim Nassif <anassif@e0r3p3.1337.ma>
2021-10-10 13:35:15 +0100

    taouil algo to understand simple static casting

--- m06/ex00/Convert.cpp
+++ m06/ex00/Convert.cpp
@@ -6,7 +6,7 @@
 /*   By: anassif <anassif@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 23:44:03 by anassif           #+#    #+#             */
-/*   Updated: 2021/10/08 14:57:34 by anassif          ###   ########.fr       */
+/*   Updated: 2021/10/10 13:34:25 by anassif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,82 @@ void    setValue(std::string value)
 void    print_it()
 {
     
+}
+
+// taouil algo
+
+
+Class::operator char()
+{
+	try
+	{
+		char c = std::stoi(_str);
+		if (!std::isprint(c))
+			throw NotPrintableException();
+		std::cout << "char: '"<<c<<"'"<<std::endl;
+		return c;
+	}
+	catch(NotPrintableException &e){
+		std::cout << e.what() << std::endl;
+	}
+	catch(std::exception &e){
+		std::cout << "char: impossible" << std::endl;
+	}
+	return 0;
+}
+
+Class::operator int ()
+{
+	try
+	{
+		int i = std::stoi(this->_str);
+		std::cout << "int: " << i << std::endl;
+		return i;
+	}
+	catch(std::exception &e){
+		std::cout << "int: impossible" << std::endl;
+	}
+	return 0;
+}
+
+Class::operator float()
+{
+	try
+	{
+		float f = std::stof(_str);
+		if (f != (int)f)
+			std::cout << "float: " << f << "f" << std::endl;
+		else
+			std::cout << "float: " << f << ".0f" << std::endl;
+		return f;
+	}
+	catch(std::exception &e){
+		std::cout << "float: impossible" <<std::endl;
+	}
+	return 0;
+}
+
+Class::operator double()
+{
+	try
+	{
+		double d = std::stod(_str);
+		if (d != (int)d)
+			std::cout << "double: " << d << std::endl;
+		else
+			std::cout << "double: " << d << ".0"<<std::endl;
+	}
+	catch(std::exception &e){
+		std::cout << "double: impossible" << std::endl;
+	}
+	return 0;
+}
+
+void	Class::Scalar()
+{
+	(void)static_cast<char>(*this);
+	(void)static_cast<int>(*this);
+	(void)static_cast<float>(*this);
+	(void)static_cast<double>(*this);
+	
 }

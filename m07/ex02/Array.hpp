3df0c2d234fd495962443da5e44bc1edd709648a
Abdelhakim Nassif <anassif@e0r1p2.1337.ma>
2021-10-15 15:54:26 +0100

    ex02 start

--- m07/ex02/Array.hpp
+++ m07/ex02/Array.hpp
@@ -5,3 +5,33 @@
 template <typename T>
 
 class array
+{
+    T *arr;
+    unsigned int lengh;
+
+    array()
+    {  
+        try
+        {
+            arr = new T;
+        }
+        catch(std::bad_alloc &e)
+        {
+            std::cout << "bad_alloc caught: " << e.what() << std::endl;
+        }
+    }
+    array(unsigned int i)
+    {
+        try
+        {
+            arr = new T[i];
+        }
+        catch(std::bad_alloc &e)
+        {
+            std::cout << "bad_alloc caught: " << e.what() << std::endl;
+        }
+    }
+    
+      
+
+};

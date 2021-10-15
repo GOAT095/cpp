7ae7f469e2d072b918825c3537482679b6d3cf82
Abdelhakim Nassif <anassif@e0r1p2.1337.ma>
2021-10-15 16:16:56 +0100

    ex02 start

--- m07/ex02/Array.hpp
+++ m07/ex02/Array.hpp
@@ -4,34 +4,50 @@
 
 template <typename T>
 
-class array
+class Array
 {
     T *arr;
     unsigned int lengh;
 
-    array()
+    Array()
     {  
         try
         {
-            arr = new T;
+            arr = new T(); // () so takes the default value
         }
         catch(std::bad_alloc &e)
         {
             std::cout << "bad_alloc caught: " << e.what() << std::endl;
         }
     }
-    array(unsigned int i)
+
+    Array(unsigned int i)
     {
         try
         {
-            arr = new T[i];
+            arr = new T[i]();
         }
         catch(std::bad_alloc &e)
         {
             std::cout << "bad_alloc caught: " << e.what() << std::endl;
         }
     }
-    
-      
+    Array(Array const &obj)
+    {
+        *this = obj;
+    }
+    Array operator = (const Brain& obj)
+    {
+        this->lengh = obj.lengh;
+        for(int i = 0 ; i < 100 ; i++)
+        {
+            arr[i] = obj.arr[i];
+        }
+        return *this;
+    }
+
+    ~Array(){std::cout << "BOOOM\n";}
+
+
 
 };
---
+++ m07/ex02/main.cpp
@@ -0,0 +1,40 @@
+#include "Array.hpp"
+
+int main()
+{
+    // std::cout << " ****** empty test ****** \n";
+    // Array<std::string> arr0;
+    // std::cout << arr0;
+    // std::cout << " ****** string test ****** \n";
+    // int size = 5;
+    // Array<std::string> arr(size);
+    // int i;
+    // for (i = 0 ; i < size; i++)
+    //     arr[i] = "hamza";
+    // std::cout << arr;
+
+    // std::cout << " ****** int test ****** \n";
+    // int size1 = 5;
+    // Array<char> arr1(size1);
+    // for (i = 0 ; i < size1; i++)
+    //     arr1[i] = 'a' + i;
+    // std::cout << arr1;
+
+    // std::cout << " ****** copy test ****** \n";
+    // Array<char>arr3(arr1);
+    // std::cout << arr3;
+    // arr1[0] = 'z';
+    // std::cout << "\n" << arr1;
+
+    // arr0 = arr;
+    // std::cout << arr0;
+
+    // std::cout << " ****** out of range test ****** \n";
+    // try{
+    //     std::cout << arr1[9];
+    // } catch (std::out_of_range &e) {
+    //     std::cout << e.what();
+    // }
+
+    return 0;
+}

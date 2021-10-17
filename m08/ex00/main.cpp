73d0e1a404e48847b9543a477d01ba4970bd8467
Abdelhakim Nassif <anassif@e2r13p9.1337.ma>
2021-10-17 21:21:10 +0100

    eawea

--- m08/ex00/main.cpp
+++ m08/ex00/main.cpp
@@ -1,24 +1,15 @@
-#include "easyfind.hpp"
+#include "span.hpp"
 #include <vector>
 #include <map>
 #include <list>
-//hchorfi's main
 
 int main()
 {
-    std::cout << "****** vector test ******\n";
     std::vector<int> myVect;
-    
+
     for (int i = 0; i < 10 ; i++){
         myVect.push_back(i);
     }
-    try
-    {
-        easyfind< std::vector<int> >(myVect, 5);
-    }
-    catch(std::string &e)
-    {
-        std::cout << e << '\n';
-    }
+    easyfind< std::vector<int> >(myVect, 5);
     return 0;
 }

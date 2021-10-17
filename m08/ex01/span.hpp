ede86f7bb24d141f145567dc2db45308142ad75c
GOAT095 <hakimoxxx@gmail.com>
2021-10-17 14:19:21 +0100

    ex00m08

---
+++ m08/ex01/span.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Span
+{
+    private:
+        unsigned int N;
+        std::vector<int> myVect;
+        unsigned int Size;
+        Span();
+
+    public:
+        Span(unsigned int n);
+        Span(const Span& copy);
+        Span& operator = (const Span& copy);
+        ~Span();
+        void    addNumber(int number);
+        int shortestSpan();
+        int longestSpan();
+};

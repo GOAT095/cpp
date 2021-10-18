db86e4bff7ad613a4951e2e28fda80583ae0333b
GOAT095 <hakimoxxx@gmail.com>
2021-10-18 13:25:38 +0100

    ex01

---
+++ m08/ex01/span.cpp
@@ -0,0 +1,52 @@
+#include "span.hpp"
+
+Span::Span(unsigned int n) : N(n){
+    Size = myVect.size();
+}
+
+void    Span::addNumber(int number){
+    try{
+        if (myVect.size() < N)
+            myVect.push_back(number);
+        else
+            throw std::string ("you reach the size limite\n");
+    } catch (std::string &e){
+        std::cout << e;
+    }
+}
+
+Span::Span(const Span& obj)
+{
+    *this = obj;
+}
+
+Span& Span::operator = (const Span& obj)
+{
+    N = obj.N;
+
+    for (std::size_t i = 0; i < obj.myVect.size(); i++)
+    {
+        myVect.push_back(obj.myVect[i]);
+    }
+    return *this;
+}
+
+int Span::shortestSpan()
+{
+    int diff = INT_MAX;
+    std::sort(myVect.begin(), myVect.end());
+    for (size_t i = 0; i < myVect.size() - 1; i++)
+    {
+        if (myVect[i + 1] - myVect[i] < diff)
+            diff = myVect[i + 1] - myVect[i];
+    }
+    return diff;
+}
+
+int Span::longestSpan()
+{
+    std::sort(myVect.begin(), myVect.end());
+    return myVect[myVect.size() - 1] - myVect[0];
+}
+
+Span::~Span(){}
